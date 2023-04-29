cfe26586602ce91223fb8e611b9b7c623dfc6429
Dev Patel <dev220791@gmail.com>
2023-04-29 09:56:53 +0600

    Cors solv7

--- frontend/src/pages/DashboardLogin.js
+++ frontend/src/pages/DashboardLogin.js
@@ -103,6 +103,7 @@ function DashboardLogin() {
                 id="email-address"
                 name="email"
                 type="email"
+                autoComplete="email"
                 onChange={handleChange}
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
@@ -118,6 +119,7 @@ function DashboardLogin() {
                 name="password"
                 type="password"
                 onChange={handleChange}
+                autoComplete="current-password"
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
               />
